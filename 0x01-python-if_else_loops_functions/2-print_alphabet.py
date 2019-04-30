a769a05b2efa64b4a55e1ca008850b52c3167b32
Tu Vo <tuvo1106@gmail.com>
2019-04-30 19:08:42 +0000

    Refactor 2

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,2 +1,4 @@
 #!/usr/bin/python3
-print("".join([chr(i) for i in range(ord('a'), ord('z') + 1)]), end="")
+#print("".join([chr(i) for i in range(ord('a'), ord('z') + 1)]), end="")
+for i in range(ord('a'), ord('z') + 1):
+    print(chr(i), end="")
