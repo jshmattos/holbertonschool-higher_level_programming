5871b62985f297dabdb59bce74f963f92b3568d6
Tu Vo <tuvo1106@gmail.com>
2019-04-30 17:05:43 +0000

    Add 0x01 first commits

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    res = ""
+    for c in str:
+        if ord(c) in range(97, 123):
+            res += chr(ord(c) - 32)
+        else:
+            res += c
+    print(res)
