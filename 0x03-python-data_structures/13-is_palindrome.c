1bf2eb5c5c62f9cae0400a71f1baba2e44b1d2df
Tu Vo <tuvo1106@gmail.com>
2019-05-07 21:18:29 +0000

    Refactor 13

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -7,86 +7,80 @@
  */
 int is_palindrome(listint_t **head)
 {
-	static int arr[50];
-	int i = 0, j = 0;
+	listint_t *slow = *head, *fast = *head, *prev = *head;
+	listint_t *second = NULL, *mid = NULL;
+	int res = 0;
+
+	if (*head)
+	{
+		while (fast && fast->next)
+		{
+			fast = fast->next->next;
+			prev = slow;
+			slow = slow->next;
+		}
+		if (fast)
+		{
+			mid = slow;
+			slow = slow->next;
+		}
+		second = slow;
+		prev->next = NULL;
+		reverse_linked_list(&second);
+		res = compare_linked_list(*head, second);
+		reverse_linked_list(&second);
+		if (mid)
+		{
+			prev->next = mid;
+			mid->next = second;
+		} else
+			prev->next = second;
+	}
+	return (res);
+}
+
+/**
+ * reverse_linked_list - reverses linked list
+ * @head: double pointer to head of list
+ */
+void reverse_linked_list(listint_t **head)
+{
+	listint_t *prev = NULL;
 	listint_t *current = *head;
+	listint_t *next;
 
-	if (!*head)
-		return (1);
 	while (current)
 	{
-		arr[i] = current->n;
-		current = current->next;
-		i++;
+		next = current->next;
+		current->next = prev;
+		prev = current;
+		current = next;
 	}
-	i--;
-	while (j <= i)
-	{
-		if (arr[j] != arr[i])
-			return (0);
-		j++;
-		i--;
-	}
-	return (1);
-
-}
-/*
-	register int i, len;
-	listint_t *start, *end;
-
-	if (!*head)
-		return (1);
-	i = 0;
-	len = listint_len(*head) - 1;
-
-	while (i < len)
-	{
-		start = get_nodeint_at_index(*head, i);
-		end = get_nodeint_at_index(*head, len);
-		if (start->n != end->n)
-			return (0);
-		i++;
-		len--;
-	}
-	return (1);
-*/
-/**
- * listint_len - returns the number of elements in a
- * linked listint_t list
- * @h: pointer to head of list
- * Return: number of elements
- */
-size_t listint_len(const listint_t *h)
-{
-	register size_t count = 0;
-
-	while (h)
-	{
-		h = h->next;
-		count++;
-	}
-	return (count);
+	*head = prev;
 }
 
 /**
- * get_nodeint_at_index - returns the nth node of a listint_t linked list
- * @head: pointer to head of list
- * @index: index of value to be returned
- * Return: address of node at input index
+ * compare_linked_list - compares two linked lists
+ * @head1: pointer to head of first list
+ * @head2: pointer to head of second list
+ * Return: 1 if true, 0 if false
  */
-listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+int compare_linked_list(listint_t *head1, listint_t *head2)
 {
-	listint_t *current;
+	listint_t *temp1 = head1;
+	listint_t *temp2 = head2;
 
-	register uint count = 0;
-
-	current = head;
-	while (current)
+	while (temp1 && temp2)
 	{
-		if (count == index)
-			return (current);
-		count++;
-		current = current->next;
+		if (temp1->n == temp2->n)
+		{
+			temp1 = temp1->next;
+			temp2 = temp2->next;
+		}
+		else
+			return (0);
 	}
-	return (current);
+	if (temp1 == NULL && temp2 == NULL)
+		return (1);
+	return (0);
 }
