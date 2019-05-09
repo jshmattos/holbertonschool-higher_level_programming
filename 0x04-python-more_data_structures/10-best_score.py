3b331f00cb1a14240755525e4eaa24f20ad00e84
Tu Vo <tuvo1106@gmail.com>
2019-05-09 23:37:20 +0000

    Refactor 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary:
-        return max(sorted(a_dictionary.items(),
-                          key=lambda x: x[1],
-                          reverse=True))[0]
+        return max(a_dictionary, key=a_dictionary.get)
