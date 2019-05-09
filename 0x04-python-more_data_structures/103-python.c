200216cf28095db66a474daab7088b447e893910
Tu Vo <tuvo1106@gmail.com>
2019-05-09 22:54:50 +0000

    Update 13

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -53,7 +53,7 @@ void print_python_list(PyObject *p)
 	for (i = 0; i < size; i++)
 	{
 		printf("Element %i: %s\n", i, (list->ob_item[i])->ob_type->tp_name);
-		if (strcmp(p[i]->tp_name, "bytes") == 0)
+		if (strcmp((list->ob_item[i])->ob_type->tp_name, "bytes") == 0)
 			print_python_bytes(list->ob_item[i]);
 	}
 }
