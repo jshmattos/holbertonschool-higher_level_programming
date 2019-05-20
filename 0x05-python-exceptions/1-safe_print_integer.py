04597e3bf7a666784037cde3aa86d8dc2ba73730
Tu Vo <tuvo1106@gmail.com>
2019-05-20 19:42:46 +0000

    Debug 100

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -5,6 +5,6 @@ def safe_print_integer(value):
     is_int = True
     try:
         print("{:d}".format(value))
-    except ValueError:
+    except TypeError:
         is_int = False
     return is_int
--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -2,17 +2,11 @@
 
 import sys
 
-
 def safe_print_integer_err(value):
     is_int = True
     try:
         print("{:d}".format(value))
-    except ValueError:
-        sys.stderr.write(
-            "Exception: Unknown format code 'd' for object of type ")
-        if isinstance(value, str):
-            sys.stderr.write("'str'\n")
-        elif isinstance(value, float):
-            sys.stderr.write("'float'\n")
+    except Exception as e:
+        print("Exception:", e, file=sys.stderr)
         is_int = False
     return is_int
