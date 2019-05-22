e9fa2a5d39dddfb3987d330ded64a095036437d1
Tu Vo <tuvo1106@gmail.com>
2019-05-22 15:14:19 +0000

    Add 100

---
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -0,0 +1,48 @@
+#!/bin/bash/python3
+
+"""
+This module contains the following functions:
+    - matrix_mul
+
+"""
+
+def matrix_mul(m_a, m_b):
+    """Multiplies 2 matrices."""
+    if not isinstance(m_a, list):
+        raise TypeError('m_a must be a list')
+    if not isinstance(m_b, list):
+        raise TypeError('m_b must be a list')
+    if not len(m_a):
+        raise ValueError("m_a can't be empty")
+    if not len(m_b):
+        raise ValueError("m_b can't be empty")
+    if not all(isinstance(x, list) for x in m_a):
+        raise TypeError('m_a must be a list of lists')
+    if not all(isinstance(x, list) for x in m_b):
+        raise TypeError('m_b must be a list of lists')
+    if any(not len(x) for x in m_a):
+        raise ValueError("m_a can't be empty")
+    if any(not len(x) for x in m_b):
+        raise ValueError("m_b can't be empty")
+    for row in m_a:
+        if not all(isinstance(num, (int, float)) for num in row):
+            raise TypeError('m_a should contain only integers or floats')
+    for row in m_b:
+        if not all(isinstance(num, (int, float)) for num in row):
+            raise TypeError('m_b should contain only integers or floats')
+    len_rows = list(map(lambda x: len(x), [x for x in m_a]))
+    if len_rows.count(len_rows[0]) != len(len_rows):
+        raise TypeError('each row of m_a must should be of the same size')
+    len_rows = list(map(lambda x: len(x), [x for x in m_b]))
+    if len_rows.count(len_rows[0]) != len(len_rows):
+        raise TypeError('each row of m_b must should be of the same size')
+    if len(m_a[0]) != len(m_b):
+        raise ValueError("m_a and m_b can't be multiplied")
+
+    matrix = [[0 for y in range(len(m_a[0]))] for row in range(len(m_a))]
+
+    for i in range(len(m_a)):
+        for j in range(len(m_b[0])):
+            for k in range(len(m_b)):
+                matrix[i][j] += m_a[i][k] * m_b[k][j]
+    return matrix
