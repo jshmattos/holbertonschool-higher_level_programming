bd11d6868934c7556ec941f4fa4f724202c6f330
Tu Vo <tuvo1106@gmail.com>
2019-05-22 18:16:27 +0000

    Update 102

--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -17,7 +17,7 @@ void print_python_string(PyObject *p)
 	printf("[.] string object info\n");
 	if (strcmp(p->ob_type->tp_name, "str") != 0)
 	{
-		printf("ERROR] Invalid String Object\n");
+		printf("  [ERROR] Invalid String Object\n");
 		return;
 	}
 	length = ((PyASCIIObject *)(p))->length;
