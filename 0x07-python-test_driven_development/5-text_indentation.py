26e493d4c76090f538a42073bd944982f6e9d72b
Tu Vo <tuvo1106@gmail.com>
2019-05-22 10:15:06 +0000

    Add 0x07

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,13 @@
+#!/bin/bash/python3
+
+"""
+This module contains the following functions:
+    - text_indentation
+
+"""
+
+def text_indentation(text):
+    """
+    Print a text with 2 new lines after each of these characters: ., ? and :
+    """
+    pass
