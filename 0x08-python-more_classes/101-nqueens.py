96d87550173df60c19c556984941e9b4d08fd73b
Tu Vo <tuvo1106@gmail.com>
2019-05-28 02:06:28 +0000

    Pep8 all

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -15,19 +15,18 @@ if __name__ == '__main__':
         sys.exit(1)
     try:
         size = int(sys.argv[1])
-    except:
+    except BaseException:
         print("N must be a number")
         sys.exit(1)
     if size < 4:
         print("N must be at least 4")
         sys.exit(1)
 
-
     def get_board(size):
         """Returns an n by n board"""
-        board = [0]*size
+        board = [0] * size
         for ix in range(size):
-            board[ix] = [0]*size
+            board[ix] = [0] * size
         return board
 
     def print_solutions(solutions, size):
@@ -49,7 +48,7 @@ if __name__ == '__main__':
     def is_safe(board, row, col, size):
         """Check if it's safe to place a queen at board[x][y]"""
 
-        #check row on left side
+        # check row on left side
         for iy in range(col):
             if board[row][iy] == 1:
                 return False
@@ -58,33 +57,33 @@ if __name__ == '__main__':
         while ix >= 0 and iy >= 0:
             if board[ix][iy] == 1:
                 return False
-            ix-=1
-            iy-=1
+            ix -= 1
+            iy -= 1
 
-        jx, jy = row,col
+        jx, jy = row, col
         while jx < size and jy >= 0:
             if board[jx][jy] == 1:
                 return False
-            jx+=1
-            jy-=1
+            jx += 1
+            jy -= 1
 
         return True
 
     def solve(board, col, size):
         """Use backtracking to find all solutions"""
-        #base case
+        # base case
         if col >= size:
             return
 
         for i in range(size):
             if is_safe(board, i, col, size):
                 board[i][col] = 1
-                if col == size-1:
+                if col == size - 1:
                     add_solution(board)
                     board[i][col] = 0
                     return
-                solve(board, col+1, size)
-                #backtrack
+                solve(board, col + 1, size)
+                # backtrack
                 board[i][col] = 0
 
     def add_solution(board):
@@ -100,5 +99,3 @@ if __name__ == '__main__':
     print_solutions(solutions, size)
 
     # print("Total solutions = {}".format(len(solutions)))
-
-
