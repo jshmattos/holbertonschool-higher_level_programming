be7c72d95bab591c86464df8cbe3269c037663ce
Tu Vo <tuvo1106@gmail.com>
2019-06-05 18:28:30 +0000

    Add all mandatory

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 
+"""
+This is a module for MyList.
+"""
+
+
 class MyList(list):
+    """A MyList class."""
 
     def print_sorted(self):
+        """Print the list, but sorted (ascending sort)."""
         print(sorted(self))
--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+
+"""
+This is a module for Square.
+"""
+
+Rectangle = __import__('9-rectangle.py').Rectangle
+
+
+class Square(Rectangle):
+    """A class square."""
+
+    def __init__(self, size):
+        """Initialize Square class."""
+        super().__init__(size, size)
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """Return the area of the square."""
+        return self.__size ** 2
