13f936e6def026c3286bae2c661ae89b52e31b40
Tu Vo <tuvo1106@gmail.com>
2019-06-06 21:37:07 +0000

    Add up to 16

--- 0x0B-python-input_output/14-pascal_triangle.py
+++ 0x0B-python-input_output/14-pascal_triangle.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+
+"""
+This is a module for pascal_triangle.
+"""
+
+from math import factorial as fc
+
+def pascal_triangle(n):
+    return [[int(fc(i)/(fc(e) * fc(i - e))) for e in range(i + 1)] for i in range(n)]
