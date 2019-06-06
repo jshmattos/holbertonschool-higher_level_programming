4358ac4818e9b9b1cfd80ba68d15c08caf7ff3ee
Tu Vo <tuvo1106@gmail.com>
2019-06-06 20:05:04 +0000

    Add up to 13

--- 0x0B-python-input_output/10-class_to_json.py
+++ 0x0B-python-input_output/10-class_to_json.py
@@ -1,2 +1,16 @@
 #!/usr/bin/python3
-def append_after(filename="", search_string="", new_string=""):
+
+"""
+This is a module for class_to_json.
+"""
+
+def class_to_json(obj):
+    """returns the dictionary description with simple data structure
+    (list, dictionary, string, integer and boolean) for JSON serialization
+    of an object:"""
+    if hasattr(obj, "__dict__"):
+        return obj.__dict__
+    if hasattr(obj, "__slots__"):
+        return obj.__slots__
+
+
--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,1 +1,31 @@
 #!/usr/bin/python3
+
+import sys
+import signal
+from time import sleep
+
+file_size = 0
+stats = {"file_size": 0, "200": 0, "301": 0, "400": 0, "401": 0, "403": 0, "404": 0, "405": 0, "500": 0}
+counter = 0
+
+def print_stats(stats : dict) -> None:
+    print("File size: {}".format(file_size))
+    stat_arr = sorted(stats.keys())
+    for num in stat_arr:
+        if stats[num]:
+            print(str(num) + ": " + str(stats[num]))
+
+def signal_handler(signum, frame):
+    print_stats(stats)
+
+signal.signal(signal.SIGINT, signal_handler)
+for line in sys.stdin:
+    counter += 1
+    data = line.split()
+    status_code = data[7]
+    stats[status_code] += 1
+    file_size += int(data[8])
+    if counter % 10 == 0:
+        print_stats(stats)
+
+
