0579943ef1802f21ad6568a06f49c6aeff4938a4
Tu Vo <tuvo1106@gmail.com>
2019-06-06 22:58:50 +0000

    Update 14

--- 0x0B-python-input_output/14-pascal_triangle.py
+++ 0x0B-python-input_output/14-pascal_triangle.py
@@ -6,7 +6,9 @@ This is a module for pascal_triangle.
 
 from math import factorial as fc
 
-
 def pascal_triangle(n):
+    """Return a list of lists of integers representing the Pascal’s triangle of n"""
+    if n <= 0:
+        return []
     return [[int(fc(i) / (fc(e) * fc(i - e)))
              for e in range(i + 1)] for i in range(n)]
