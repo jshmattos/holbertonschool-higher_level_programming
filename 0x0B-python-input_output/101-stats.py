d9fd67ec923e5f962cde9354d8dfce4e8343d093
Tu Vo <tuvo1106@gmail.com>
2019-06-07 01:02:20 +0000

    Add conditional check before adding to dict in 101

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -29,12 +29,13 @@ if __name__ == '__main__':
     try:
         for line in sys.stdin:
             counter += 1
-            if counter % 10 == 0:
-                print_stats(stats, file_size)
             data = line.split()
             status_code = data[7]
-            stats[status_code] += 1
+            if status_code in stats:
+                stats[status_code] += 1
             file_size += int(data[8])
+            if counter % 10 == 0:
+                print_stats(stats, file_size)
         print_stats(stats, file_size)
     except KeyboardInterrupt:
         print_stats(stats, file_size)
