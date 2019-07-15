3cb2b8da2a3b20306c3f110dcefb6a6cc7a4bc7c
Tu Vo <tuvo1106@gmail.com>
2019-07-15 19:13:42 +0000

    Capitalize 101 and 103

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,3 +1,3 @@
 -- displays the average temperature (Fahrenheit) by city ordered by temperature (descending)
 -- displays the average temperature (Fahrenheit) by city ordered by temperature (descending)
-select city, AVG(value) as avg_temp from temperatures GROUP BY city ORDER BY avg_temp DESC;
+SELECT city, AVG(value) AS avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp DESC;
--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,2 +1,2 @@
 -- displays the max temperature of each state (ordered by State name)
-select state, MAX(value) as max_temp from temperatures GROUP BY state ORDER BY state;
+SELECT state, MAX(value) AS max_temp FROM temperatures GROUP BY state ORDER BY state;
