26b938b40d535c8e777423fca1a8b9e5b917f298
Tu Vo <tuvo1106@gmail.com>
2019-07-16 18:20:10 +0000

    Add assignments up to 13

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -2,6 +2,9 @@
 -- creates database hbtn_0d_2
 -- create user user_0d_2@localhost
 -- grants select permissions to user on database hbtn_0d_2
-CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
- GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
+       CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+  CREATE USER IF NOT EXISTS user_0d_2@localhost
+IDENTIFIED BY 'user_0d_2_pwd';
+        GRANT SELECT
+	   ON hbtn_0d_2.*
+	   TO user_0d_2@localhost;
