939d1b558495b411a233b2b60ea1e72519ae0093
Tu Vo <tuvo1106@gmail.com>
2019-07-16 21:16:12 +0000

    Update 100 to sort

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -6,6 +6,7 @@ LEFT JOIN tv_show_genres
        ON tv_genres.id = tv_show_genres.genre_id
 LEFT JOIN tv_shows
        ON tv_show_genres.show_id = tv_shows.id
+ ORDER BY name
     WHERE name NOT IN (
 	SELECT name
 	  FROM tv_genres
