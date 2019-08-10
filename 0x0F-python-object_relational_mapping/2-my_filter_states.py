69f667e108a74797a4dcdd2d1802a9e6a09c8a7c
Tu Vo <tuvo1106@gmail.com>
2019-08-10 06:01:02 +0000

    Update pep8 on task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -18,7 +18,8 @@ if __name__ == '__main__':
                          db=db_name,
                          charset="utf8")
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE BINARY name='{}'".format(state_name) +
+    cur.execute("SELECT * FROM states WHERE BINARY " +
+                "name='{}' ".format(state_name) +
                 "ORDER BY id ASC")
     rows = cur.fetchall()
     for row in rows:
