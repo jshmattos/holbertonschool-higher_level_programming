883bf9b592f023b6b727ae63692f7a8aa7d4eacc
Tu Vo <tuvo1106@gmail.com>
2019-08-20 21:21:15 +0000

    Upload first draft of task 100

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Sends a request to a URL passed as an argument, and displays only the status code
-# of the response.
+# Sends a request to a URL and displays only the status code
+curl -LI "$1" -o /dev/null -w '%{http_code}\n' -s
