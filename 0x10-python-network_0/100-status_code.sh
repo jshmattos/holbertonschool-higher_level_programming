95d7dbd9f4f32007bfb273d97fafd1736ef5e56e
Tu Vo <tuvo1106@gmail.com>
2019-08-20 22:04:52 +0000

    Add silent flag in all curl scripts

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends a request to a URL and displays only the status code
-curl -LI "$1" -o /dev/null -w '%{http_code}' -s
+curl -s -L -I "$1" -o /dev/null -w '%{http_code}'
--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Makes a request to 0.0.0.0:5000/catch_me that causes the server response
-curl -L -H "Origin: HolbertonSchool" -X PUT -d "user_id=98" "$1"
+curl -s -L -H "Origin: HolbertonSchool" -X PUT -d "user_id=98" 0.0.0.0:5000/catch_me
