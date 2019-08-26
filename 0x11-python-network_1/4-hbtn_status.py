9b4f055bedb520179ed29cbd069d834941d48d8b
Tu Vo <tuvo1106@gmail.com>
2019-08-26 16:52:05 +0000

    Add assignment number 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+
+"""
+Fetches https://intranet.hbtn.io/status
+"""
+
+import requests
+
+if __name__ == "__main__":
+    response = requests.get("https://intranet.hbtn.io/status")
+    print("Body response:")
+    print("    - type: {}".format(type(response.text)))
+    print("    - content: {}".format(response.text))
