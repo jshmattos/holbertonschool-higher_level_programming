4c3d3bfee9ee0867e4c5ae819073f8f998833333
Tu Vo <tuvo1106@gmail.com>
2019-08-26 19:23:31 +0000

    Update task 0 with tabs

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -10,6 +10,6 @@ if __name__ == "__main__":
     with urllib.request.urlopen("https://intranet.hbtn.io/status") as f:
         r = f.read()
         print("Body response:")
-        print("    - type: {}".format(type(r)))
-        print("    - content: {}".format(r))
-        print("    - utf8 content: {}".format(r.decode('utf-8')))
+        print("\t- type: {}".format(type(r)))
+        print("\t- content: {}".format(r))
+        print("\t- utf8 content: {}".format(r.decode('utf-8')))
--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+#!/usr/bin/python3
+
+"""
+Takes in a string and sends a search request to the Star Wars API
+"""
+
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    r = requests.get("https://swapi.co/api/people/?search=" + argv[1])
+    if "json" not in r.headers['content-type']:
+        print("Not a valid JSON")
+    else:
+        res = r.json()
+        print("Number of results: " + str(res.get('count')))
+        for r in res['results']:
+            print(r.get('name'))
