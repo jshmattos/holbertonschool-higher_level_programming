47345561a06ff0a578566cbb42dfb56db8f70d27
Tu Vo <tuvo1106@gmail.com>
2019-09-23 18:33:43 +0000

    Semi all js files

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
-const { dict } = require('./101-data')
+const { dict } = require('./101-data');
 
 console.log(Object.entries(dict).reduce((d, el) => {
   !d[el[1]]
     ? d[el[1]] = [el[0]]
-    : d[el[1]].push(el[0])
-  return d
-}, {}))
+    : d[el[1]].push(el[0]);
+  return d;
+}, {}));
