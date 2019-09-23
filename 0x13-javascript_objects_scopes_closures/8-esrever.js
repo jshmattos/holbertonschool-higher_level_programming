f42f5f9e39bd61b1836d90200310ad58356ba8a9
Tu Vo <tuvo1106@gmail.com>
2019-09-23 18:31:59 +0000

    Initial commit on closures

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+exports.esrever = function (list) {
+  return list.reduce((arr, el) => {
+    arr.unshift(el);
+    return arr;
+  }, []);
+};
---
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+exports.logMe = (function (item) {
+  let counter = 0;
+  return function (item) {
+    console.log(`${counter}: ${item}`);
+    counter += 1;
+  };
+}());
