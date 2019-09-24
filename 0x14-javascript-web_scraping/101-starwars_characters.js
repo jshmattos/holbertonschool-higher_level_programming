e20d49b8c66ceba765bec1fbb860302ea2e37926
Tu Vo <tuvo1106@gmail.com>
2019-09-24 17:07:03 +0000

    Upload task 101

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,1 +1,24 @@
 #!/usr/bin/node
+
+const request = require('request');
+
+const req = (arr, i) => {
+  if (i === arr.length) { return; }
+  request(arr[i], (err, response, body) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(JSON.parse(body).name);
+      req(arr, i + 1);
+    }
+  });
+};
+
+request(`http://swapi.co/api/films/${process.argv[2]}`, (err, response, body) => {
+  if (err) {
+    console.log(err);
+  } else {
+    const chars = JSON.parse(body).characters;
+    req(chars, 0);
+  }
+});
