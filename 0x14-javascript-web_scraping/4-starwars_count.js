e3ccbbd647530b362b1ef9f04edef2f9455de344
Tu Vo <tuvo1106@gmail.com>
2019-09-24 18:18:34 +0000

    Refactor task 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,7 +2,6 @@
 
 const request = require('request');
 
-const wedge = 'https://swapi.co/api/people/18/';
 request(`${process.argv[2]}`, (err, response, body) => {
   if (err) {
     console.log(err);
@@ -10,10 +9,10 @@ request(`${process.argv[2]}`, (err, response, body) => {
     const films = JSON.parse(body).results;
     console.log(films.reduce((count, el) => {
       el.characters.forEach(e => {
-	if (e.includes('18')) {
-	  count++;
-	}
-      })
+        if (e.includes('18')) {
+          count++;
+        }
+      });
       return count;
     }, 0));
   }
